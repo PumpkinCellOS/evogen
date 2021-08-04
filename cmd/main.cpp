e142a8d8c43d930de78337d0d72647dcd72bd9de
Maciej Zygmanowski <sppmacd@pm.me>
2021-08-04 14:15:33 +0200

    Get rid of absolute positions
    Now all positions are turtle-relative.

--- cmd/main.cpp
+++ cmd/main.cpp
@@ -6,12 +6,12 @@ int main()
 {
     evo::Generator generator;
     generator.add_task<evo::PlaceBlockTask>(evo::Block("chest", "facing=south", R"({Items:[{id:diamond,Count:64}]})"),
-                            evo::BlockPosition::create_absolute({0, 0, 0}));
+                            evo::BlockPosition{{0, 0, 0}});
     generator.add_task<evo::FillBlocksTask>(evo::Block("grass_block"),
-                            evo::BlockPosition::create_relative({1, 1, 1}),
-                            evo::BlockPosition::create_relative({5, 5, 5})
+                            evo::BlockPosition{{1, 1, 1}},
+                            evo::BlockPosition{{5, 5, 5}}
     );
-    generator.add_task<evo::MoveTurtleTask>(evo::BlockPosition::create_relative({0, 5, 0}), generator.turtle());
+    generator.add_task<evo::MoveTurtleTask>(evo::BlockPosition{{0, 5, 0}}, generator.turtle());
     generator.generate_to_stdout();
     return 0;
 }
--- libevogen/Block.h
+++ libevogen/Block.h
@@ -31,34 +31,28 @@ private:
 class BlockPosition
 {
 public:
-    static BlockPosition create_absolute(Vector<int> const& position) { return BlockPosition(position, false); }
-    static BlockPosition create_relative(Vector<int> const& position) { return BlockPosition(position, true); };
-
-    bool is_relative() const { return m_relative; }
+    BlockPosition(Vector<int> const& position)
+    : m_position(position) {}
 
     std::string to_command_format() const
     {
-        return  coord_to_string(m_position.x, m_relative) + " " +
-                coord_to_string(m_position.y, m_relative) + " " +
-                coord_to_string(m_position.z, m_relative);
+        return  coord_to_string(m_position.x) + " " +
+                coord_to_string(m_position.y) + " " +
+                coord_to_string(m_position.z);
     }
 
     Vector<int> resolve_relative_position(Vector<int> const& other) const
     { 
-        return m_relative ? (other + m_position) : m_position;
+        return other + m_position;
     }
 
 private:
-    static std::string coord_to_string(int value, bool relative)
+    static std::string coord_to_string(int value)
     {
-        return (relative ? "~" : "") + std::to_string(value);
+        return "~" + std::to_string(value);
     }
 
-    BlockPosition(Vector<int> const& position, bool relative)
-    : m_position(position), m_relative(relative) {}
-
     Vector<int> m_position;
-    bool m_relative = false;
 };
 
 }
