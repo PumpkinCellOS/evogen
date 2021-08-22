fe442939e6a8100bd652056a12baadee952b33d8
Maciej Zygmanowski <sppmacd@pm.me>
2021-08-22 15:58:29 +0200

    Turtle: Teleport player to turtle spawn position

--- libevogen/Turtle.cpp
+++ libevogen/Turtle.cpp
@@ -5,6 +5,10 @@ namespace evo
 
 void Turtle::generate_spawn_command(std::ostream& stream) const
 {
+    // Teleport player(s) that load chunks to start position
+    stream << "tp @e[tag=evogen] " << m_start_position.to_string() << std::endl;
+
+    // Actually summon the armor stand
     stream << "summon armor_stand " << m_start_position.to_string() << R"( {Tags:["evogen"],NoGravity:1,Invisible:1,CustomName:"\")" + m_custom_name + R"(\""})" << std::endl;
 }
 
